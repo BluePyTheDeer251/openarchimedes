07580a303d3f4302b9bbf65218e571e260a26761
Paul <bluepythewolfdeer@proton.me>
2024-10-05 11:28:33 -0600

    Optimized and made the thing better

--- calc.py
+++ calc.py
@@ -2,28 +2,10 @@ print("Welcome to my calculator app (codename: archimedes)")
 
 #The calculator
 def process():
-  calculating_type = input("What do you want to calculate? Addition(1), Subtraction(2), Multiplication(3), or Division?(4) ")
-  if calculating_type == 1:
-      part1 = input("First number: ")
-      part2 = input("Second number: ")
-      result = int(part1) + int(part2)
-      print(result)
-  if calculating_type == 2:
-      part1 = input("First number: ")
-      part2 = input("Second number: ")
-      result = int(part1) - int(part2)
-      print(result)
-  if calculating_type == 3:
-      part1 = input("First number: ")
-      part2 = input("Second number: ")
-      result = int(part1) * int(part2)
-      print(result)
 
-  if calculating_type == 4:
-      part1 = input("First number: ")
-      part2 = input("Second number: ")
-      result = int(part1) / int(part2)
-      print(result)
+calculation = input("Type the problem here: ")
+total = int(calculation)
+print(calculation)
 
 process()
       
