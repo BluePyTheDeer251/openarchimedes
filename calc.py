f3aa160636c4d24e1609f06cb284e0556da79114
Paul <bluepythewolfdeer@proton.me>
2024-10-17 12:37:59 -0600

    Fixed a flaw

--- calc.py
+++ calc.py
@@ -2,10 +2,9 @@ print("Welcome to my calculator app (codename: archimedes)")
 
 #The calculator
 def process():
-
-calculation = input("Type the problem here: ")
-total = float(calculation)
-print(calculation)
+  calculation = input("Type the problem here: ")
+  total = float(calculation)
+  print(calculation)
 
 process()
       
