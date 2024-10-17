6ef2d173e6a8380b05358c6dae2cf7cbdd102524
Paul <bluepythewolfdeer@proton.me>
2024-10-17 13:09:40 -0600

    Added NumPy

--- calc.py
+++ calc.py
@@ -1,9 +1,11 @@
+import numpy as np
+
 print("Welcome to my calculator app (codename: archimedes)")
 
 #The calculator
 def process():
   calculation = input("Type the problem here: ")
-  total = float(calculation)
+  total = ""
   print(calculation)
 
 process()
