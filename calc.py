4533b23565029a3c144388d5c30f1302ab6480e4
BluePy <bluepythewolfdeer@proton.me>
2024-11-03 14:18:34 -0600

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 print("Welcome to my calculator app (codename: archimedes)")
-
+# idk when will I start working on this again but it will possibly be on December
 #The calculator
 def process():
   calculation = input("Type the problem here: ")
